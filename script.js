0cf5f1a8ef5fcb35b0d9ed7332d576cf255433df
SilvaBrunoR <79067409+SilvaBrunoR@users.noreply.github.com>
2026-03-27 21:42:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const form = document.getElementById("rsvpForm");
 const btn = document.getElementById("submitBtn");
 const msg = document.getElementById("statusMsg");
 
-form.addEventListener("submit", () => {
-  btn.disabled = true;
-  btn.textContent = "Sending...";
-  msg.textContent = "Submitting your RSVP...";
-});
+if (form) {
+  form.addEventListener("submit", () => {
+    btn.disabled = true;
+    btn.textContent = "Sending...";
+    msg.textContent = "Submitting your RSVP...";
+  });
+}
 
 // COUNTDOWN
-const partyDate = new Date("2026-03-14T10:00:00").getTime();
+const partyDate = new Date("2026-04-11T11:00:00").getTime();
 
 const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
@@ -27,7 +29,7 @@ function updateCountdown() {
     hoursEl.textContent = "0";
     minutesEl.textContent = "0";
     secondsEl.textContent = "0";
-    countdownMessage.textContent = "The dinosaur party is here! 🎉";
+    countdownMessage.textContent = "The birthday day is here! 🎉";
     return;
   }
 
